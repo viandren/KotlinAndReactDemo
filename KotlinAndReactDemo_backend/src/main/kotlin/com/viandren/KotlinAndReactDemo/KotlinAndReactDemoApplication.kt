8502fac31c90d02a6ffd4a0ccfbac33187f1aa05
Robert Frank <viandren.home@gmail.com>
2023-01-13 21:56:18 +0100

    init backend

---
+++ KotlinAndReactDemo_backend/src/main/kotlin/com/viandren/KotlinAndReactDemo/KotlinAndReactDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.viandren.KotlinAndReactDemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinAndReactDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinAndReactDemoApplication>(*args)
+}
---
+++ KotlinAndReactDemo_backend/src/test/kotlin/com/viandren/KotlinAndReactDemo/KotlinAndReactDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.viandren.KotlinAndReactDemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinAndReactDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
